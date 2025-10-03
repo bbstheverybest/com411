7b867a1dc889f1283b5a034282c85aa65bff7a6e
BEN BROOKS-SMITH <0broob88@solent.ac.uk>
2025-10-03 14:44:42 +0100

    User is asked what they like, different responses spur different answers.

--- week2/if_statements.py
+++ week2/if_statements.py
@@ -1,8 +1,18 @@
 print("What type of books are you into?")
-if("Adventure Books"):
+book=input()
+if book=="adventure":
 
    print("I like adventure books!!!")
 print("Finished reading book.")
+
+print("Enter an activity")
+activity=input()
+if activity=="calculate":
+   print("Performing calculations...")
+
+else:
+   print("Performing activity...")
+print("Activity completed!")
 
 
 
