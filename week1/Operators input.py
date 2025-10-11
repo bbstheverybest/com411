a795e83ec73126fe761aa4e4adfa3bf1407d7188
bbstheverybest <bbs01ben@gmail.com>
2025-10-11 23:13:00 +0100

    Mini game input task

---
+++ week1/Operators input.py
@@ -0,0 +1,15 @@
+print("Please enter the number of lives")
+lives= int(input())
+
+print("Please enter the energy level")
+energy= int(input())
+
+print("Please enter the shield level")
+shield= int(input())
+
+print()
+print("Health has been set.")
+
+print(f"Lives: {'❤' * lives}")
+print(f"Energy: {'❇' * energy}")
+print(f"Shield: {'✚' * shield}")
