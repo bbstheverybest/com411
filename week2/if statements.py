d67268596b6f1ad4ad383d1abb51d0008afb0084
bbstheverybest <bbs01ben@gmail.com>
2025-10-12 18:30:15 +0100

    Added cide ti demonstrate the use of an if...elif..else statement

---
+++ week2/if statements.py
@@ -0,0 +1,12 @@
+print("Towards which direction should I go (up, down, left or right?")
+direction=input()
+if "up":
+    print("up")
+print()
+print("I am moving in an upward direction.")
+
+
+
+
+
+
