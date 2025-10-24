708f6456ea7663cf4996691bd015c1d6243b1618
bbstheverybest <bbs01ben@gmail.com>
2025-10-24 17:38:26 +0100

    Added code to demonstrate how a variable can be used as a counter

---
+++ week2/Counter.py
@@ -0,0 +1,26 @@
+print("Please enter a number")
+number=int(input())
+print("Please enter a second number")
+second_number=int(input())
+print("Please enter a third number")
+third_number=int(input())
+
+even_numbers = 0
+odd_numbers=0
+
+if number % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+
+if second_number % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+
+if third_number % 2 == 0:
+    even_numbers = even_numbers + 1
+else:
+    odd_numbers = odd_numbers + 1
+
+print(f"(There were {even_numbers} even and {odd_numbers} odd)")
