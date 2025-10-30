faf83cd6ace6ec354881c3171315bce1d65cc5ea
bbstheverybest <bbs01ben@gmail.com>
2025-10-30 19:23:12 +0000

    Added code to demonstrate the use of nested loops and decisions

---
+++ week3/Sequences_of_text.py
@@ -0,0 +1,18 @@
+print("Enter a sequence of characters consisting of dashes and two markers.")
+sequence = input()
+
+print("Please enter the character for the marker:")
+marker = input()
+
+marker1_position = -1
+marker2_position = -1
+
+for position in range(0, len(sequence), 1):
+    letter = sequence[position]
+
+    if letter == marker:
+        if (marker1_position == -1):
+            marker1_position = position
+        else:
+            marker2_position = position
+print(f"The distance between the markers is {marker2_position - marker1_position -1}.")
