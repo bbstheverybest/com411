699c796b83849939fe787dcba9c504eb68d5ba10
bbstheverybest <bbs01ben@gmail.com>
2025-10-30 19:16:51 +0000

    Added code to demonstrate the use of one loop nested in another

---
+++ week3/nested_loops.py
@@ -0,0 +1,11 @@
+print("Print how many rows should i have?")
+rows = int(input())
+print()
+print("How many columns should I have?")
+columns = int(input())
+print()
+
+for row in range(0, rows, 1):
+    for column in range(0, columns, 1):
+        print(":D", end="")
+    print()
