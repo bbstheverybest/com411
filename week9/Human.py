06fe9c1142cfe919fd5345a5d2133ffda16abf0e
BEN BROOKS-SMITH <0broob88@solent.ac.uk>
2025-11-21 14:17:37 +0000

    Upadted classes human and robot with methods to display objects.

--- week9/Human.py
+++ week9/Human.py
@@ -8,8 +8,12 @@ class Human:
 
     def display(self):
         print(f"I am {self.name}.")
-
+    def __repr__(self):
+        return f'human(name={self.name}, age={self.age})'
+    def __str__(self):
+        return f'Human ({self.name} is {self.age} years old.'
 
 if __name__ == "__main__":
      human = Human()
-     human.display()
+     #human.display()
+     print(human)
--- week9/robot.py
+++ week9/robot.py
@@ -7,7 +7,13 @@ class Robot:     #Creating robot class.
 
     def display(self):       #Display self, and all that is contained in self.
         print(f"I am {self.name}.")       #Printing I am - whatever is in self.name.
+    def __repr__ (self):
+        return f'robot(name={self.name}, age={self.age})'
+    def __str__ (self):
+        return f'Robot({self.name}, is {self.age} years old.'
 
 if __name__ == "__main__":    #Testing class.
-    robot = Robot()           #Calling the class.
-    robot.display()           #Displaying class.
+    robot = Robot()     #Calling the class.
+    #robot.display()     #Displaying class.
+    print(robot)
+
