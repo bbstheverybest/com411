7606c5004911b8b605b2f42d1a698b99cca01421
BEN BROOKS-SMITH <0broob88@solent.ac.uk>
2025-11-21 14:49:40 +0000

    Updated classes human and robot with instance methods

--- week9/Human.py
+++ week9/Human.py
@@ -11,9 +11,22 @@ class Human:
     def __repr__(self):
         return f'human(name={self.name}, age={self.age})'
     def __str__(self):
-        return f'Human ({self.name} is {self.age} years old.'
+        return f'Human ({self.name} is {self.age} years old, and has {self.energy} energy)'
+
+    def grow(self):
+        self.age = 0
+
+    def eat(self):
+        self.energy = 5
+
+    def move(self, distance):
+        self.energy = max(self.energy - distance, 0)
+
+
 
 if __name__ == "__main__":
      human = Human()
      #human.display()
+     print(human)
+     human.move(50)
      print(human)
